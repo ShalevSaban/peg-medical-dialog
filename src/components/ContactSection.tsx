65e488b07b604dd8d563d0c40493934ef62a91ca
ShalevSaban <shalevsaban1@gmail.com>
2025-10-19 14:15:11 +0300

    change mail

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -107,10 +107,10 @@ export const ContactSection = () => {
                   <div>
                     <div className="font-semibold mb-2 text-lg">Email Us</div>
                     <a
-                      href="mailto:peg-medical@gmail.com"
+                      href="mailto:pegmedical1@gmail.com"
                       className="text-white/90 hover:text-white transition-colors text-base hover:underline"
                     >
-                      peg-medical@gmail.com
+                      pegmedical1@gmail.com
                     </a>
                   </div>
                 </div>
